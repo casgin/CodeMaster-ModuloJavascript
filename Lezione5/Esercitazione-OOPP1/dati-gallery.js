ff3d79ec7009ba8a9259f2a657f8b07b8c83cd78
gcastro <gianfranco.castro@gmail.com>
2016-10-14 13:28:36 +0200

    Esempi JSON

---
+++ Lezione5/Esercitazione-OOPP1/dati-gallery.js
@@ -0,0 +1,61 @@
+var jsonPhotoGallery = {
+	"elencoImmagini" : [
+
+		{
+			"nomefile" : "foto1.jpg",
+			"titolo" : "Titolo foto 1",
+			"data" : "oggi",
+			"descrizione" : "bla bla bla",
+			"tag" : ['milano', '', 'torre unicredi'],
+			"metadata" : {
+				"luogoScatto" : "",
+				"latitudine" : "",
+				"longitudine" : "",
+				"modelloMacchina" : "Nikon 123"
+			}
+ 		},
+		{
+			"nomefile" : "foto-milano-1.png",
+			"titolo" : "Non lo so",
+			"data" : "ieri",
+			"descrizione" : "bellissima foto di mialno",
+			"tag" : ['milano', '', 'torre unicredi']
+ 		},
+		{
+			"nomefile" : "castello.jpg",
+			"titolo" : "Il castello",
+			"data" : "nel medioevo",
+			"descrizione" : "Bellisismo castello mal frequntato",
+			"tag" : ['milano', '', 'torre unicredi']
+ 		},
+		{
+			"nomefile" : "",
+			"titolo" : "",
+			"data" : "",
+			"descrizione" : "",
+			"tag" : ['milano', '', 'torre unicredi']
+ 		},
+		{
+			"nomefile" : "",
+			"titolo" : "",
+			"data" : "",
+			"descrizione" : "",
+			"tag" : ['milano', '', 'torre unicredi']
+ 		},
+		{
+			"nomefile" : "",
+			"titolo" : "",
+			"data" : "",
+			"descrizione" : "",
+			"tag" : ['milano', '', 'torre unicredi']
+ 		},
+		{
+			"nomefile" : "",
+			"titolo" : "",
+			"data" : "",
+			"descrizione" : "",
+			"tag" : ['milano', '', 'torre unicredi']
+ 		},
+
+	]
+};
---
+++ Lezione5/Esercitazione-OOPP1/dati.js
@@ -0,0 +1,26 @@
+var collezioneBrani = {
+
+	"autore": "Vasco Rossi",
+	"titolo": "Alba Chiara",
+	"anno" : 1986,
+	"nomeAlbum" : "Alba Chiara Album",
+	"componentiBand" : [
+		{
+			"nome" : "Ciccio",
+			"strumento" : "Batteria"
+		},
+		{
+			"nome" : "Luigi",
+			"strumento" : "Basso"
+		},
+		{
+			"nome" : "Tizio",
+			"strumento" : "1ma chitarra"
+		},
+		{
+			"nome" : "Quello li",
+			"strumento" : "2nd chitarra"
+		}
+	]
+
+};
